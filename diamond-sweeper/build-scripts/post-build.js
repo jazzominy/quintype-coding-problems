1b1fbe3bd76f8111d56fb7ba6153341578765da1
Jasmine Hirpara <jasmine758@gmail.com>
2018-09-25 19:30:27 +0530

    added build script

---
+++ diamond-sweeper/build-scripts/post-build.js
@@ -0,0 +1,20 @@
+var express =  require("express");
+var path = require("path");
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("dist"));
+
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+app.listen(port, function(err) {
+	if(err) {
+		console.log(err);
+	}
+	else {
+		console.log(`dist server running at http://localhost:${port}`);
+	}
+});
